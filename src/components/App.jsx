1818545a6433e4349988c9e577689698d36ddace
Aleksandr2905 <natalukha@ukr.net>
2023-09-27 22:56:59 +0300

    style task01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
     //   justifyContent: 'center',
     //   alignItems: 'center',
     //   fontSize: 40,
-    //   color: '#010101'
+    //   color: '#010101',
     // }}
     >
       <Profile
@@ -26,7 +26,6 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
+import styles from './Profile.module.css';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
-       
-    return (
-        <div className="profile">
-            <div className="description">
-                <img
-                    src={avatar}
-                    alt="User avatar"
-                    className="avatar"
-                />
-                <p className="name">{username}</p>
-                <p className="tag">@{tag}</p>
-                <p className="location">{location}</p>
-            </div>
+  return (
+    <div className={styles.profile}>
+      <div className={styles.description}>
+        <img src={avatar} alt="User avatar" className={styles.avatar} />
+        <p className={styles.name}>{username}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
+      </div>
 
-            <ul className="stats">
-                <li>
-                    <span className="label">Followers</span>
-                    <span className="quantity">{stats.followers}</span>
-                </li>
-                <li>
-                    <span className="label">Views</span>
-                    <span className="quantity">{stats.views}</span>
-                </li>
-                <li>
-                    <span className="label">Likes</span>
-                    <span className="quantity">{stats.likes}</span>
-                </li>
-            </ul>
-        </div>
-    )
+      <ul className={styles.stats}>
+        <li className={styles.item}>
+          <span className={styles.label}>Followers</span>
+          <span className={styles.quantity}>{stats.followers}</span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Views</span>
+          <span className={styles.quantity}>{stats.views}</span>
+        </li>
+        <li className={styles.item}>
+          <span className={styles.label}>Likes</span>
+          <span className={styles.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
-
-
-
-
