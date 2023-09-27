71535db80fd1a6d7ad8dd175ffe253aa58eb1cbb
Aleksandr2905 <natalukha@ukr.net>
2023-09-27 20:05:32 +0300

    task04

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,35 @@
-import { Profile } from "./profile/Profile";
+import { Profile } from './profile/Profile';
 import user from './profile/user.json';
-import { Statistics } from "./statistics/Statistics";
+import { Statistics } from './statistics/Statistics';
 import data from './statistics/data.json';
-import { FriendList } from "./friendList/FriendList";
+import { FriendList } from './friendList/FriendList';
 import friends from './friendList/friends.json';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+import transactions from './transactionHistory/transactions.json';
 
 export const App = () => {
   return (
     <div
-      // style={{
-      //   height: '100vh',
-      //   display: 'flex',
-      //   justifyContent: 'center',
-      //   alignItems: 'center',
-      //   fontSize: 40,
-      //   color: '#010101'
-      // }}
+    // style={{
+    //   height: '100vh',
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   fontSize: 40,
+    //   color: '#010101'
+    // }}
     >
-      <Profile username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats} />
-    
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
       <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends}/>
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
